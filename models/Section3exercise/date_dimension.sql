2d1b668168acce882cf8a344cd746ba2a5b19ed4
chirio d4w2i <chirio.d4w2i@slmail.me>
2026-02-25 16:55:24 +0000

    date_dimension model

---
+++ models/Section3exercise/date_dimension.sql
@@ -0,0 +1,30 @@
+with cte as (
+    select 
+        --started_at,
+        to_timestamp(started_at) as started_at,
+        date(to_timestamp(started_at)) as date,
+        month(to_timestamp(started_at)) as month,
+        hour(to_timestamp(started_at)) as hour,
+        dayname(to_timestamp(started_at)) as dayofweek,
+        case
+            when dayofweek in ('Sat', 'Sun')
+            then 'WEEKEND'
+            ELSE 'BUSINESSDAY'
+            END
+            AS day_type,
+        case
+            when month in (12,1,2) then 'WINTER'
+            when month in (3,4,5) then 'SPRING'
+            when month in (6,7,8) then 'SUMMER'
+            when month in (9,10,11) then 'AUTUMN'
+            ELSE '' END
+            AS season_of_year,
+    from 
+    {{ source('demo', 'bike') }}
+    --where
+    --started_at <> 'started_at'
+) 
+
+select 
+* 
+from cte

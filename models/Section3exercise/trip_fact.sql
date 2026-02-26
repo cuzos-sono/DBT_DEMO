93b108f1cb2cbf74f207f1705ce6ff804869438b
chirio d4w2i <chirio.d4w2i@slmail.me>
2026-02-26 17:41:44 +0000

    TRIP FACT TABLE MODEL

---
+++ models/Section3exercise/trip_fact.sql
@@ -0,0 +1,25 @@
+with 
+SOURCE as (
+    select 
+        *
+    from 
+    {{ source('demo', 'bike') }}
+    -- limit 10
+),
+
+TRIPS as (
+    select 
+    RIDE_ID,
+    RIDEABLE_TYPE,
+    DATE(STARTED_AT) AS TRIP_DATE,
+    START_STATIO_ID AS START_STATION_ID,
+    END_STATION_ID,
+    MEMBER_CSUAL AS MEMBER_CASUAL,
+    TIMESTAMPDIFF(SECOND, STARTED_AT, ENDED_AT) AS TRIP_DURATION_SEC
+    from SOURCE
+)
+
+
+select 
+* 
+from TRIPS
